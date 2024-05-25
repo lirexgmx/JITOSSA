ff74fc4fdea8e7cc3daee43f5d2f2ac0a7d9d757
lirexgmx <nxonagraom@gmail.com>
2024-05-25 02:15:06 +0100

    Update config.js

--- config.js
+++ config.js
@@ -9,20 +9,20 @@ import moment from 'moment-timezone';
 
 global.botnumber = ""
 //يمكنك إضافة رقمك للتحكم في البوت أو إن طلب شخص رقمك من البوت
-global.owner = [
-  ['212670941551', 'Omar', true],
+global.ABDEESAMAD = [
+  ['212716407654', 'ABDO', true],
   
 ];
 
-global.suittag = ['212670941551'];
-global.prems = ['212670941551'];
+global.suittag = ['212716407654'];
+global.prems = ['212716407654'];
 
-global.packname = 'JITOSSA';
+global.packname = 'GANARA';
 global.author = '';
-global.wm = 'OMAR';
-global.titulowm = 'JITOSSA';
-global.titulowm2 = `JITOSSA`
-global.igfg = 'JITOSSA';
+global.wm = 'ABDO';
+global.titulowm = 'GANARA';
+global.titulowm2 = `GANARA`
+global.igfg = 'GANARA';
 global.wait = '> _جاري معالجة طلبك حاول الإنتظار ..._\n\n *متابعة للمطور في إنستجرام* \n*_instagram.com/ovmar_1_*';
 global.waitt = '> سوف أجيب عن سؤالك بعد قليل ...';
 global.waittt = '> سوف يتم رسم صورتك بعد قليل ...';
@@ -47,12 +47,12 @@ global.mes = d.toLocaleDateString('en', {month: 'long'});
 global.año = d.toLocaleDateString('en', {year: 'numeric'});
 global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true});
 //* ****************************
-global.wm2 = `${dia} ${fecha}\nJITOSSA`;
-global.gt = 'JITOSSA';
-global.mysticbot = 'JITOSSA';
+global.wm2 = `${dia} ${fecha}\nGANARA`;
+global.gt = 'GANARA';
+global.mysticbot = 'GANARA';
 global.md = 'https://instagram.com/ovmar_1';
 global.mysticbot = 'https://instagram.com/ovmar_1';
-global.nomorown = '212670941551';
+global.nomorown = '212716407654';
 global.pdoc = ['application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/msword', 'application/pdf', 'text/rtf'];
 global.cmenut = '❖––––––『';
 global.cmenub = '┊✦ ';
